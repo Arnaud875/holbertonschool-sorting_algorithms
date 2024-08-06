f5912895057871ab729937b14ca056ce4c0e5e76
Arnaud875 <171556021+Arnaud875@users.noreply.github.com>
2024-08-06 19:02:25 +0200

    Update task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,20 +8,30 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int temp;
+	int temp, swapped;
 
-	for (size_t i = 0; i < size; i++)
+	if (!array || size < 2)
+		return;
+
+	for (size_t i = 0; i < size - 1; i++)
 	{
-		for (size_t j = 0; j < (size - i); j++)
+		swapped = 0;
+
+		for (size_t j = 0; j < (size - i - 1); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+
+				swapped = 1;
 			}
 
 			print_array(array, size);
 		}
+
+		if (!swapped)
+			break;
 	}
 }
